8b716d1bea226e2e752abb750a841669017a0bd8
Fernando Rojo <frojo@sas.upenn.edu>
2020-03-11 13:57:40 -0400

    🔥fb config

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,7 +2,9 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const db = !firebase.apps.length
-	? firebase.initializeApp().firestore()
+	? firebase.initializeApp({
+	  // config here...
+	}).firestore()
 	: firebase.app().firestore()
 
 export const fuego = { db }
