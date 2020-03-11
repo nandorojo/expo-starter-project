2a1a12f9df0a326b297db60e47deddabbb8a5b52
Fernando Rojo <frojo@sas.upenn.edu>
2020-03-11 13:56:18 -0400

    🔥

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,13 +1,3 @@
-// export const fuego = new Fuego({
-// 	apiKey: 'AIzaSyBYA23RoUNQx_JRiE_QYbonur2bKne-e7U',
-// 	authDomain: 'themicdrop-f0c9e.firebaseapp.com',
-// 	databaseURL: 'https://themicdrop-f0c9e.firebaseio.com',
-// 	projectId: 'themicdrop-f0c9e',
-// 	storageBucket: 'themicdrop-f0c9e.appspot.com',
-// 	messagingSenderId: '661922928136',
-// 	appId: '1:661922928136:web:76ccc74cd6d4b26d85a3bd',
-// 	measurementId: 'G-XVC4NNVMZ9',
-// })
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
